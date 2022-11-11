1d5f5d81a3204053a7e0060b2dc5967784113e1e
Wase-Zahin <w.zahin@yahoo.com>
2022-11-12 01:23:41 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import styled from "styled-components"
 
-const FooterContainer = styled.div`
+const FooterContainer = styled.footer`
     display: grid;
     background-color: #393E46;
-    grid-template-columns: repeat(auto, minmax(300px, 1fr)); 
+    color: white;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); 
 `
 const Footer = () => {
     return (
@@ -15,6 +16,31 @@ const Footer = () => {
                     Ut elit tellus, luctus nec 
                     ullamcorper mattis, 
                     pulvinar dapibus leo.</p>
+            </div>
+            <div>
+                <h3>Contact</h3>
+                <ul>
+                    <li>My account</li>
+                    <li>Order History</li>
+                    <li>Wishlist</li>
+                    <li>Privacy Policy</li>
+                    <li>Frequently Asked</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Information</h3>
+                <ul>
+                    <li>Delivery Information</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms & Conditions</li>
+                    <li>Contact us</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Sign Up For Newsletters</h3>
+                <p>Be the First to Know. Sign up for newsletter today !</p>
+                <input placeholder="Enter your email address"></input>
             </div>
         </FooterContainer>
     )
